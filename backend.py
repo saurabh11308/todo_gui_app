c9f0b5fbbbab3ad463441279eb40d4e1069588d3
Saurabh <saurabh11308@yahoo.com>
2026-07-11 16:21:52 +0530

    Implemented the backend module to interface with the file

---
+++ backend.py
@@ -0,0 +1,14 @@
+FILEPATH = "todos1.txt"
+
+def get_todos(filepath=FILEPATH):
+    """This function or routine is used to read the todos from the
+    FILEPATH and return it"""
+    with open(filepath) as file:
+        todos_local = file.readlines()
+    return todos_local
+
+def set_todos(todos_arg,filepath=FILEPATH):
+    """This function or routine is used to write the todos list
+    supplied to the file mentioned in FILEPATH"""
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
