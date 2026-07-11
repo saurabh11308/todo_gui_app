62f46cd74c2aeb02d38dacf3d04f0901421c1bae
Saurabh <saurabh11308@yahoo.com>
2026-07-11 16:32:23 +0530

    Implemented the add button

--- gui.py
+++ gui.py
@@ -1,14 +1,27 @@
 import FreeSimpleGUI as sg
+from backend import get_todos,set_todos
 
 sg.theme("Reddit")
 
 label = sg.Text("This is a to-do app")
-ibox = sg.InputText(tooltip="Enter todo :-")
+ibox = sg.InputText(tooltip="Enter todo :-",key='ibox')
 add_button = sg.Button("Add")
-
 layout = [[label],[ibox,add_button]]
+window = sg.Window("Todo App",layout=layout,font=('Calibara',10))
 
-window = sg.Window("Todo App",layout=layout,font=('Calibara',20))
+while True:
+    event,values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = get_todos()
+            add_value = values['ibox'] + '\n'
+            todos.append(add_value)
+            set_todos(todos)
+        case sg.WINDOW_CLOSED:
+            exit()
+
 
 window.read()
 window.close()
